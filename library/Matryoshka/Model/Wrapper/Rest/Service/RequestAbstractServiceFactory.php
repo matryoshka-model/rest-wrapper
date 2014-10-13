ff26f63a9fdd242d3a7aceb933b7c0870a091499
visa4 <antonino.visalli@gmail.com>
2014-10-13 18:52:37 +0200

    Refactor

--- library/Matryoshka/Model/Wrapper/Rest/Service/RequestAbstractServiceFactory.php
+++ library/Matryoshka/Model/Wrapper/Rest/Service/RequestAbstractServiceFactory.php
@@ -1,35 +1,22 @@
 <?php
-/**
- * REST matryoshka wrapper
- *
- * @link        https://github.com/matryoshka-model/rest-wrapper
- * @copyright   Copyright (c) 2014, Ripa Club
- * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
- */
 namespace Matryoshka\Model\Wrapper\Rest\Service;
 
-use Matryoshka\Model\Wrapper\Rest\RestClient;
-use Zend\Http\Client;
-use Zend\Http\Headers;
 use Zend\Http\Request;
+use Zend\Http\Headers;
 use Zend\ServiceManager\AbstractFactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-/**
- * Class ApiAbstractServiceFactory
- */
-class RestClientAbstractServiceFactory implements AbstractFactoryInterface
+class RequestAbstractServiceFactory implements AbstractFactoryInterface
 {
     /**
      * @var string
      */
-    protected $configKey = 'rest-api'; // TODO: choose correct config node name
+    protected $configKey = 'rest-api-request'; // TODO: choose correct config node name
 
     /**
      * @var array
      */
     protected $config;
-
     /**
      * Determine if we can create a service with name
      *
@@ -46,11 +33,11 @@ class RestClientAbstractServiceFactory implements AbstractFactoryInterface
         }
 
         $serviceConfig = $this->checkHasRequestedNameConfig($config, $requestedName);
-        $requestNodeConfig = $this->checkHasRequestedNameConfig($config, $requestedName, $serviceLocator);
+        $urlNodeConfig = $this->checkHasUrlConfig($config, $requestedName);
 
         return (
             $serviceConfig
-            && $requestNodeConfig
+            && $urlNodeConfig
         );
     }
 
@@ -66,31 +53,17 @@ class RestClientAbstractServiceFactory implements AbstractFactoryInterface
     {
         $config = $this->getConfig($serviceLocator)[$requestedName];
 
-        $httpClient = new Client();
-        $request = $serviceLocator->get($config['request']);
-
-        $restClient = new RestClient($httpClient, $request);
+        $request = new Request();
+        $request->setUri($config['url']);
 
         // Array of int code valid
-        if (isset($config['codesStatusValid'])) {
-            $restClient->setCodesStatusValid($config['codesStatusValid']);
-        }
-        // Int 0/1
-        if (isset($config['returnType'])) {
-            $restClient->setReturnType($config['returnType']);
-        }
-        // string json/xml
-        if (isset($config['formatResponse'])) {
-            $restClient->setFormatResponse($config['formatResponse']);
-        }
-        // Profiler
-        if ($serviceLocator->has($config['profiler'])) {
-            $restClient->setProfiler($serviceLocator->get($config['profiler']));
+        if (isset($config['headers'])) {
+            $headers = new Headers();
+            $request->setHeaders($headers->addHeaders($config['headers']));
         }
 
-        return $restClient;
+        // TODO ADD TOKEN SERVICE
     }
-
     /**
      * Get rest configuration, if any
      *
@@ -137,22 +110,20 @@ class RestClientAbstractServiceFactory implements AbstractFactoryInterface
     }
 
     /**
-     * Check if has node config request
+     * Check if has node config url
      *
      * @param $config
      * @param $requestedName
-     * @param ServiceLocatorInterface $serviceLocator
      * @return bool
      */
-    public function checkHasDefaultRequestConfig($config, $requestedName, ServiceLocatorInterface $serviceLocator)
+    public function checkHasUrlConfig($config, $requestedName)
     {
-        if (isset($config[$requestedName]['request'])
-            && is_string($config[$requestedName]['request'])
-            && !empty($config[$requestedName]['request'])
-            && $serviceLocator->has($config[$requestedName]['request'])
+        if (isset($config[$requestedName]['url'])
+            && is_string($config[$requestedName]['url'])
+            && !empty($config[$requestedName]['url'])
         ) {
             return true;
         }
         return false;
     }
-}
+} 
