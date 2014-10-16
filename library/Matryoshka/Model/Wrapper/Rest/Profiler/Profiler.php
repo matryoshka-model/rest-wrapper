91184afb830f30eb09d8833c3d47069fb2fd3219
visa4 <antonino.visalli@gmail.com>
2014-10-16 17:23:12 +0200

    Fix

--- library/Matryoshka/Model/Wrapper/Rest/Profiler/Profiler.php
+++ library/Matryoshka/Model/Wrapper/Rest/Profiler/Profiler.php
@@ -44,13 +44,7 @@ class Profiler implements ProfilerInterface
         ];
 
         if ($target instanceof Request) {
-            $profileInformation['request']['uri'] = $target->getUriString();
-            $profileInformation['request']['headers'] = $target->getHeaders();
-            $profileInformation['request']['method'] = $target->getMethod();
-            $profileInformation['request']['post'] = $target->getPost()->toString();
-            $profileInformation['request']['query'] = $target->getPost()->toString();
-            $profileInformation['request']['content'] = $target->getContent();
-            $profileInformation['request']['toString'] = $target->getContent()->toString();
+            $profileInformation['request']['toString'] = $target->toString();
         }
 
         $this->profiles[$this->currentIndex] = $profileInformation;
@@ -69,13 +63,7 @@ class Profiler implements ProfilerInterface
         $current['elapse'] = $current['end'] - $current['start'];
 
         if ($target instanceof Response) {
-            $profileInformation['response']['uri'] = $target->getUriString();
-            $profileInformation['response']['headers'] = $target->getHeaders();
-            $profileInformation['response']['method'] = $target->getMethod();
-            $profileInformation['response']['post'] = $target->getPost()->toString();
-            $profileInformation['response']['query'] = $target->getPost()->toString();
-            $profileInformation['response']['content'] = $target->getContent();
-            $profileInformation['response']['toString'] = $target->getContent()->toString();
+            $profileInformation['response']['toString'] = $target->toString();
         }
 
         $this->currentIndex++;
